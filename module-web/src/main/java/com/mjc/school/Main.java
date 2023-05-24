ac2fd9daa06453eac7a828a8fc33cb337f9c7933
HelgaNovikova <helpotaki@rambler.ru>
2023-05-24 15:18:20 -0400

    make the app deploying as war

--- module-web/src/main/java/com/mjc/school/Main.java
+++ module-web/src/main/java/com/mjc/school/Main.java
@@ -2,11 +2,20 @@ package com.mjc.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        System.out.println ("Simple code of spring boot war application");
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
     }
 }
